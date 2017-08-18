4e1dbba6938ade759891faad6b27ca36eb2f79f8
zbb <zhangbinbin@ybveg.com>
2017-08-18 18:13:49 +0800

    FreeLogin

--- src/main/java/com/ybveg/auth/FreeLogin.java
+++ src/main/java/com/ybveg/auth/FreeLogin.java
@@ -1,11 +1,20 @@
 package com.ybveg.auth;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * 免登陆验证
  *
  * @auther zbb
  * @create 2017/8/11
  */
+@Target(value = {ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface FreeLogin {
 
 }
